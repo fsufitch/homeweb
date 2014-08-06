2714f5d1b172e48a4e09e4acbf9d9412a2ff3217
Filip Sufitchi <filip@scientiamobile.com>
2014-08-06 16:15:30 -0400

    bad import

--- src/homeweb/server.py
+++ src/homeweb/server.py
@@ -1,10 +1,13 @@
 from yaul.daemon import Daemon, run_as_service
 from tornado.web import Application
+from tornado.ioloop import IOLoop
 
 
 class HomeWebDaemon(Daemon):
     def run(self):
-        pass
+        application = Application([(r"/", MainHandler),])
+        application.listen(8888)
+        IOLoop.instance().start()
 
 def main_service():
     daemon = HomeWebDaemon('/tmp/homeweb.pid')
