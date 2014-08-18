d43d0d4c438321bacd2835ab006b74b92093543f
Filip Sufitchi <filip@scientiamobile.com>
2014-08-18 16:52:12 -0400

    server configuration file

---
+++ src/homeweb/config.py
@@ -0,0 +1,30 @@
+import json
+
+DEFAULT_CONFPATH = '/etc/homeweb.json'
+CONF = None
+
+def get_config(generate=False, confpath=None):
+    if not CONF:
+        if not generate:
+            raise Exception("Config not initialized!")
+        else:
+            global CONF
+            if not confpath:
+                confpath = DEFAULT_CONFPATH
+            CONF = Configuration(confpath)
+    return CONF
+
+class Configuration(object):
+    def __init__(self, confpath):
+        with open(confpath) as f:
+            self.raw = json.loads(f.read())
+
+    def get(self, *args):
+        if not args:
+            raise TypeError("Must specify at least one configuration path argument")
+        current_conf = self.raw
+        for arg in args:
+            current_conf = current_conf.get(arg)
+            if not current_conf:
+                return None
+        return current_conf
--- src/homeweb/server.py
+++ src/homeweb/server.py
@@ -1,8 +1,11 @@
+import sys
+
 from pkg_resources import resource_filename
 from yaul.daemon import Daemon, run_as_service
 from tornado.web import Application, StaticFileHandler
 from tornado.ioloop import IOLoop
 
+from homeweb.config import get_config
 from homeweb.handlers.index import IndexHandler
 from homeweb.handlers.about import AboutYouHandler
 
@@ -12,21 +15,28 @@ PATHS = [
          (r'/s/(.*)', StaticFileHandler, {'path': resource_filename(__name__, 'static')}),
          ]
 
-def runserv():
+def runserv(conf):
     application = Application(PATHS)
-    application.listen(8888)
+    application.listen(conf.get('listen', 'port'),
+                       address=conf.get('listen', 'host') or '')
     IOLoop.instance().start()
 
 class HomeWebDaemon(Daemon):
+    def __init__(self, conf):
+        super(HomeWebDaemon, self).__init__(conf.get('pidpath'))
+        self.conf = conf
+
     def run(self):
-        runserv()
+        runserv(self.conf)
 
 def main_service():
-    daemon = HomeWebDaemon('/tmp/homeweb.pid')
+    conf = get_config(generate=True)
+    daemon = HomeWebDaemon(conf)
     run_as_service(daemon)
 
 def main_cli():
-    runserv()
-
-if __name__ == '__main__':
-    main_cli()
+    confpath = None
+    if len(sys.argv)>1:
+        confpath = sys.argv[1]
+    conf = get_config(generate=True, confpath=confpath)
+    runserv(conf)
