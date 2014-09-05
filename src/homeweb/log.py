c64a510683c93dcffb27fa40b0a881d467461761
Filip Sufitchi <fsufitchi@gmail.com>
2014-09-05 17:02:49 -0400

    make access logging work

--- src/homeweb/log.py
+++ src/homeweb/log.py
@@ -9,13 +9,26 @@ ERROR = ERROR_LOG
 ACCESS = ACCESS_LOG
 
 ERROR_LOG_SETUP = False
+ACCESS_LOG_SETUP = False
+
+def make_handler(conf, log_type):
+    if conf.get(log_type, 'type')=='file':
+        fname = conf.get(log_type, 'destination')
+        if not fname:
+            raise TypeError('Configuration specifies file logging, but no path given', log_type)
+        return logging.FileHandler(fname)
+    elif conf.get(log_type, 'type')=='stream':
+        return logging.StreamHandler()
+    else:
+        raise TypeError('Invalid error log type')
+
 def setup_error_logging(conf):
     global ERROR_LOG_SETUP
     LOG = ERROR_LOG
     if ERROR_LOG_SETUP:
-        LOG.warn('Attempted to set up logging twice')
+        LOG.warn('Attempted to set up error logging twice')
         return
-    
+
     levels = {'debug': logging.DEBUG,
               'info': logging.INFO,
               'warning': logging.WARNING,
@@ -24,17 +37,8 @@ def setup_error_logging(conf):
               }
     chosen_level = levels.get(conf.get('error_log', 'level'), logging.DEBUG)
     LOG.setLevel(chosen_level)
-    
-    handler = None
-    if conf.get('error_log', 'type')=='file':
-        fname = conf.get('error_log', 'destination')
-        if not fname:
-            raise TypeError('Configuration specifies file error logging, but no path given')
-        handler = logging.FileHandler(fname)
-    elif conf.get('error_log', 'type')=='stream':
-        handler = logging.StreamHandler()
-    else:
-        raise TypeError('Invalid error log type')
+
+    handler = make_handler(conf, 'error_log')
     handler.setLevel(chosen_level)
     handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
     LOG.handlers = []
@@ -43,5 +47,23 @@ def setup_error_logging(conf):
     LOG.debug('Error log configured')
     ERROR_LOG_SETUP = True
 
+def setup_access_logging(conf):
+    global ACCESS_LOG_SETUP
+    LOG = ACCESS_LOG
+    if ACCESS_LOG_SETUP:
+        ERROR.warn('Attempted to set up access logging twice')
+        return
+    LOG.setLevel(logging.INFO)
+    handler = make_handler(conf, 'access_log')
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(message)s'))
+    LOG.handlers = []
+    LOG.propagate = False
+    LOG.addHandler(handler)
+    ERROR.debug('Access log configured')
+    ACCESS_LOG_SETUP = True
+
+
 def setup_logging(conf):
     setup_error_logging(conf)
+    setup_access_logging(conf)
--- src/homeweb/server.py
+++ src/homeweb/server.py
@@ -1,4 +1,4 @@
-import sys
+import json, time, sys
 
 from pkg_resources import resource_filename
 from yaul.daemon import Daemon, run_as_service
@@ -9,7 +9,7 @@ from homeweb.config import get_config
 from homeweb.handlers.index import IndexHandler
 from homeweb.handlers.about import AboutYouHandler, AboutMeHandler
 from homeweb.handlers.contact import ContactInfoHandler
-from homeweb.log import setup_logging, ERROR
+from homeweb.log import setup_logging, ERROR, ACCESS
 
 PATHS = [
          (r'/', IndexHandler),
@@ -19,9 +19,23 @@ PATHS = [
          (r'/s/(.*)', StaticFileHandler, {'path': resource_filename(__name__, 'static')}),
          ]
 
+def log_request(request_handler):
+    print("hello! "+str(request_handler))
+    request = request_handler.request
+    output = {
+              'time': time.time(),
+              'method': request.method,
+              'uri': request.uri,
+              'remote_ip': request.remote_ip,
+              'protocol': request.protocol,
+              'host': request.host,
+              'request_time': request.request_time(),
+              }
+    ACCESS.info(json.dumps(output))
+
 def runserv(conf):
     setup_logging(conf)
-    application = Application(PATHS)
+    application = Application(PATHS, log_function=log_request)
     port = conf.get('listen', 'port')
     host = conf.get('listen', 'host') or ''
     ERROR.info('Listening on \'%s\', port %s' % (host, port))
